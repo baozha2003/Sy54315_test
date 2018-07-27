aa8865c97bf5377b903ede14d4d80d182bd62979
JackyR <baozha2003@163.com>
2018-07-27 15:30:35 +0800

    20180727 update

--- sy54315/test_case/models/myunit.py
+++ sy54315/test_case/models/myunit.py
@@ -5,7 +5,7 @@ from .driver import browser
 class MyTest(unittest.TestCase):
 
     def setUp(self):
-        self.driver = browser('Chrome')
+        self.driver = browser('Phantomjs')
         self.driver.implicitly_wait(10)
         self.driver.maximize_window()
 
